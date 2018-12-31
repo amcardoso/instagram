bd214b950e84aae0940d6c47c6ae14d2b0bbf195
Anderson Cardoso <anderson1@br.ibm.com>
2018-12-31 08:17:38 -0200

    Commit inicial

---
+++ src/components/photo-feed.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import PhotoSection from './photo-section';
+import { connect } from 'react-redux';
+import { getPhotos } from '../actions';
+
+class PhotoFeed extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {photos: [{id: 1, username: ''}]};
+
+    
+  // }
+
+  componentDidMount() {
+    this.props.getPhotos();
+  }
+
+  renderPhotos() {
+    return this.props.photos.map(_photo => {
+      return <PhotoSection key={_photo.id} photo={_photo} />
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        {this.renderPhotos()}
+      </ScrollView>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    photos: state.photos
+  }
+}
+
+export default connect(mapStateToProps, {getPhotos})(PhotoFeed);
